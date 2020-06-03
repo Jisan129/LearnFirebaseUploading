c9105b03287222ebf956d521716d10fc0a07c3d7
Jisan129 <jishananam@iut-dhaka.edu>
2020-06-03 08:02:36 +0600

    Can Retrive the Image file and show it in a ImageView

--- app/src/main/java/com/example/learnfirebase/MainActivity.java
+++ app/src/main/java/com/example/learnfirebase/MainActivity.java
@@ -1,23 +1,36 @@
 package com.example.learnfirebase;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static  final int PICK_IMAGE=1;
     private Button chosseFile;
     private Button upload;
     private EditText editFileName;
     private TextView showFile;
     private ProgressBar progressBar;
     private ImageView imageView;
+    private Uri imageUri;
+    public static final String TAG ="Tag";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,10 +45,30 @@ public class MainActivity extends AppCompatActivity {
         chosseFile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                openFileChooser();
             }
         });
 
 
+    }
+
+    private void openFileChooser() {
+        Intent intent =new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==PICK_IMAGE&&resultCode==RESULT_OK&& data!=null && data.getData()!=null){
+            imageUri=data.getData();
+            Picasso.get().load(imageUri).fit().into(imageView);
+            Toast.makeText(getApplicationContext(),"Succesful",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(getApplicationContext(),"Not so God",Toast.LENGTH_SHORT).show();
+        }
     }
 }
