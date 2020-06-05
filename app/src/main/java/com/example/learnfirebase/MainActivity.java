a73b467bebf2cd50b2d8adbbcb40f2f1e2f176f2
Jisan129 <jishananam@iut-dhaka.edu>
2020-06-06 01:34:49 +0600

    Sakib bhai

--- app/src/main/java/com/example/learnfirebase/MainActivity.java
+++ app/src/main/java/com/example/learnfirebase/MainActivity.java
@@ -7,9 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
-import android.nfc.Tag;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.webkit.MimeTypeMap;
@@ -20,11 +18,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.Continuation;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
@@ -33,12 +27,10 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
-import java.net.URI;
-
 public class MainActivity extends AppCompatActivity {
-    public static  final int PICK_IMAGE=1;
-    public static final String TAG ="Tag";
-    private Button chosseFile;
+    public static final int PICK_IMAGE = 1;
+    public static final String TAG = "Tag";
+    private Button chooseFile;
     private Button upload;
     private EditText editFileName;
     private TextView showFile;
@@ -50,19 +42,18 @@ public class MainActivity extends AppCompatActivity {
     private StorageReference storageReference;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        chosseFile=findViewById(R.id.chooseing_btn);
-        upload=findViewById(R.id.upload_btn);
-        editFileName =findViewById(R.id.editText_chosing);
-        showFile=findViewById(R.id.file_text);
-        imageView=findViewById(R.id.imageView);
-        progressBar=findViewById(R.id.progressBar);
-        storageReference= FirebaseStorage.getInstance().getReference("Uploads");
-        databaseReference=FirebaseDatabase.getInstance().getReference("Uploads");
+        chooseFile = findViewById(R.id.chooseing_btn);
+        upload = findViewById(R.id.upload_btn);
+        editFileName = findViewById(R.id.editText_chosing);
+        showFile = findViewById(R.id.file_text);
+        imageView = findViewById(R.id.imageView);
+        progressBar = findViewById(R.id.progressBar);
+        storageReference = FirebaseStorage.getInstance().getReference("Uploads");
+        databaseReference = FirebaseDatabase.getInstance().getReference("Uploads");
 
 
         upload.setOnClickListener(new View.OnClickListener() {
@@ -73,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        chosseFile.setOnClickListener(new View.OnClickListener() {
+        chooseFile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openFileChooser();
@@ -85,72 +76,87 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void openFileChooser() {
-        Intent intent =new Intent();
+        Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(intent,PICK_IMAGE);
+        startActivityForResult(intent, PICK_IMAGE);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==PICK_IMAGE&&resultCode==RESULT_OK&& data!=null && data.getData()!=null){
-            imageUri=data.getData();
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            imageUri = data.getData();
             Picasso.get().load(imageUri).fit().into(imageView);
-            Toast.makeText(getApplicationContext(),"Succesful",Toast.LENGTH_SHORT).show();
-        }
-        else {
-            Toast.makeText(getApplicationContext(),"Not so God",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "Succesful", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Not so God", Toast.LENGTH_SHORT).show();
         }
     }
 
-    private String getFileExtension(Uri uri){
-        ContentResolver cr=getContentResolver();
-        MimeTypeMap mim =MimeTypeMap.getSingleton();
+    private String getFileExtension(Uri uri) {
+        ContentResolver cr = getContentResolver();
+        MimeTypeMap mim = MimeTypeMap.getSingleton();
         return mim.getExtensionFromMimeType(cr.getType(uri));
     }
 
     private void UploadFile() {
+        Log.i(TAG, "UploadFile: "+imageUri);
+        if (imageUri != null) {
 
-         if(imageUri!=null){
-             StorageReference fileReference =storageReference.child(System.currentTimeMillis()+"."+getFileExtension(imageUri));
-             fileReference.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                 @Override
-                 public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                     Handler handler=new Handler();
-                     handler.postDelayed(new Runnable() {
-                         @Override
-                         public void run() {
-                             progressBar.setProgress(0);
-                         }
-                     },5000);
-                     Upload upload=new Upload(editFileName.getText().toString().trim(),
-                             taskSnapshot.getUploadSessionUri().toString());
-                      String uploadId=databaseReference.push().getKey();
-                      databaseReference.child(uploadId).setValue(upload);
-                 }
+            Log.i(TAG, "UploadFile: Hello");
+            StorageReference fileReference = FirebaseStorage.getInstance().getReference().child("Jisan");
+            UploadTask uploadTask = fileReference.putFile(imageUri);
+            uploadTask
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                            Log.i(TAG, "onProgress: "+taskSnapshot.getBytesTransferred()+" / "+taskSnapshot.getTotalByteCount());
+                        }
+                    })
+                    .addOnSuccessListener(taskSnapshot -> Log.i(TAG, "UploadFile: successful"))
+                    .addOnFailureListener(e -> Log.i(TAG, "UploadFile: " + e.getLocalizedMessage()));
+        }
+        /*if(imageUri!=null){
+            StorageReference fileReference =storageReference.child(System.currentTimeMillis()+"."+getFileExtension(imageUri));
+            fileReference.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Handler handler=new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBar.setProgress(0);
+                        }
+                    },5000);
+                    Upload upload=new Upload(editFileName.getText().toString().trim(),
+                            taskSnapshot.getUploadSessionUri().toString());
+                    String uploadId=databaseReference.push().getKey();
+                    databaseReference.child(uploadId).setValue(upload);
+                }
 
 
-             }).addOnFailureListener(new OnFailureListener() {
-                 @Override
-                 public void onFailure(@NonNull Exception e) {
-                     Toast.makeText(getApplicationContext(),"Failure ",Toast.LENGTH_SHORT).show();
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getApplicationContext(),"Failure ",Toast.LENGTH_SHORT).show();
 
-                 }
-             }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                 @Override
-                 public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
-                     double progress =(100.00*taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
-                     progressBar.setProgress((int)progress);
-                 }
-             });
-         }
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                    double progress =(100.00*taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
+                    progressBar.setProgress((int)progress);
+                }
+            });
+        }*/
 
 
-        else
-            {
-                Toast.makeText(getApplicationContext(),"No files Selected",Toast.LENGTH_SHORT).show();
-            }
+        else {
+           //Log.i(TAG, "UploadFile: ");
+            Toast.makeText(getApplicationContext(), "No files Selected", Toast.LENGTH_SHORT).show();
+        }
+
 
     }
 }
